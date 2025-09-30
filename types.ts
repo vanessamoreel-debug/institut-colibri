f33ce804cb726624b02b3b299fa677deb22a51f4
vanessamoreel-debug <vanessa.moreel@gmail.com>
2025-09-30 15:04:47 +0200

    Update types.ts

--- types.ts
+++ types.ts
@@ -3,10 +3,11 @@ export type Service = {
   id: string;
   name: string;
   price: number;
-  duration?: number | null;        // minutes
-  approxDuration?: boolean | null; // true => afficher "± duration min"
+  duration?: number | null;
+  approxDuration?: boolean | null;
   category?: string | null;
   description?: string | null;
+  order?: number | null;  // <- champ ordre
 };
 
 export type Category = {
