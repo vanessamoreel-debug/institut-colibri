3090b307b5e901e75086e8c5c2bd8899d46176fb
vanessamoreel-debug <vanessa.moreel@gmail.com>
2025-10-01 09:37:35 +0200

    Create page.tsx

---
+++ app/a-propos/page.tsx
@@ -0,0 +1,12 @@
+// /app/a-propos/page.tsx
+export default function AProposPage() {
+  return (
+    <>
+      <h2>À propos</h2>
+      <p>
+        Présentation de l’Institut Colibri, votre histoire, vos valeurs, vos
+        spécialités… (texte à compléter).
+      </p>
+    </>
+  );
+}
