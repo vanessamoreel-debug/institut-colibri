943ea9ba9508a82360da8bc4c63af340f9fa3403
vanessamoreel-debug <vanessa.moreel@gmail.com>
2025-10-01 16:00:08 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "./NavBar";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Institut Colibri",
@@ -13,15 +12,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="fr">
       <body className="site">
-       <header className="site-header">
-  <h1 className="site-title">Institut Colibri</h1>
-  <NavBar />
-</header>
+        <header className="site-header">
+          <h1 className="site-title">Institut Colibri</h1>
+          <NavBar />
+        </header>
 
         <main className="site-main">{children}</main>
-       <footer className="site-footer card">
-  © {new Date().getFullYear()} Institut Colibri
-</footer>
+
+        {/* Footer fixé en bas à droite */}
+        <footer className="site-footer">
+          © {new Date().getFullYear()} Institut Colibri
+        </footer>
       </body>
     </html>
   );
